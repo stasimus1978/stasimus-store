2052ce54c76a310528f9de79c4f2fde87ff54cc1
stasimus78 <stas.od@gmail.com>
2025-03-17 16:37:18 +0200

    15. Добавить модели User, Account, Session и VerificationToken в схему Prisma, обновить скрипт начальной загрузки данных

--- db/seed.ts
+++ db/seed.ts
@@ -2,8 +2,8 @@ import { prisma } from "./prisma";
 import sampleData from "./sample-data";
 
 async function main() {
-  // await prisma.user.deleteMany();
-  // await prisma.user.createMany({ data: sampleData.users })
+  await prisma.user.deleteMany();
+  await prisma.user.createMany({ data: sampleData.users });
 
   await prisma.product.deleteMany();
   await prisma.product.createMany({ data: sampleData.products });

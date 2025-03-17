171b63a0aefb2373d839235906a8c45617a7504e
stasimus78 <stas.od@gmail.com>
2025-03-17 15:44:41 +0200

    12. Добавить страницу деталей продукта с отображением изображений, цены и статуса наличия, а также создать компонент для отображения изображений продукта и новый компонент для бейджей

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,5 +1,10 @@
 import { notFound } from "next/navigation";
 
+import ProductImages from "@/components/shared/product/product-images";
+import ProductPrice from "@/components/shared/product/product-price";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 
 type Props = { params: Promise<{ slug: string }> };
@@ -10,7 +15,73 @@ const ProductDetailsPage = async (props: Props) => {
 
   if (!product) notFound();
 
-  return <>{product.name}</>;
+  return (
+    <>
+      <section>
+        <div className="grid grid-cols-1 md:grid-cols-5">
+          {/* image block */}
+          <div className="col-span-2">
+            <ProductImages images={product.images} />
+          </div>
+
+          {/* product details block */}
+          <div className="col-span-2 p-5">
+            <div className="flex flex-col gap-6">
+              <p>
+                {product.brand} {product.category}
+              </p>
+
+              <h1 className="h3-bold">{product.name}</h1>
+
+              <p>
+                {String(product.rating)} of {product.numReviews} Review(s)
+              </p>
+
+              <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
+                <ProductPrice
+                  value={Number(product.price)}
+                  className="w-24 rounded-full bg-green-100 px-5 py-2 text-green-700"
+                />
+              </div>
+            </div>
+            <div className="mt-10">
+              <p className="font-semibold">Description</p>
+              <p>{product.description}</p>
+            </div>
+          </div>
+
+          {/* price products block */}
+          <div className="py-5">
+            <Card>
+              <CardContent className="p-4">
+                <div className="mb-2 flex justify-between">
+                  <div>Price</div>
+                  <div>
+                    <ProductPrice value={Number(product.price)} />
+                  </div>
+                </div>
+
+                <div className="mb-2 flex justify-between">
+                  <div>Status</div>
+                  {product.stock > 0 ? (
+                    <Badge variant="outline">In Stock</Badge>
+                  ) : (
+                    <Badge variant="destructive">Out of Stock</Badge>
+                  )}
+                </div>
+
+                {product.stock > 0 && (
+                  <div className="flex-center">
+                    <Button className="w-full">Add to Cart</Button>
+                  </div>
+                )}
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ProductDetailsPage;

ac2c81bfaa27b9c1f65df591b959845d320ebf78
stasimus78 <stas.od@gmail.com>
2025-03-18 19:33:35 +0200

    32. Добавить схемы валидации и модели для заказов и элементов заказа: реализовать структуру данных для обработки заказов и их элементов в приложении

--- types/index.ts
+++ types/index.ts
@@ -3,22 +3,41 @@ import { z } from "zod";
 import {
   cartItemSchema,
   insertCartSchema,
+  insertOrderItemSchema,
+  insertOrderSchema,
   insertProductSchema,
   paymentMethodSchema,
   shippingAddressSchema,
   signUpFormSchema
 } from "@/lib/validator";
 
+//
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
   rating: string;
   createdAt: Date;
 };
 
+//
 export type SignUpFormSchemaType = z.infer<typeof signUpFormSchema>;
 
+//
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 
+//
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+//
 export type PaymentMethod = z.infer<typeof paymentMethodSchema>;
+//
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: boolean;
+  paidAt: Date | null;
+  isDelivered: boolean;
+  deliveredAt: Date | null;
+  orderItems: OrderItem[];
+  user: { name: string; email: string };
+};
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;

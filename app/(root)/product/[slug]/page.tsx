8f527b5482c8d810a29e345f36514f566ba6ac9c
stasimus78 <stas.od@gmail.com>
2025-03-18 14:47:04 +0200

    25. Добавить функциональность удаления товара из корзины: реализовать проверку наличия товара в корзине, уменьшение количества или удаление товара, а также обновление корзины в базе данных

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -5,6 +5,7 @@ import ProductImages from "@/components/shared/product/product-images";
 import ProductPrice from "@/components/shared/product/product-price";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
+import { getMyCart } from "@/lib/actions/cart.actions";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 
 type Props = { params: Promise<{ slug: string }> };
@@ -14,6 +15,8 @@ const ProductDetailsPage = async (props: Props) => {
   const product = await getProductBySlug(slug);
 
   if (!product) notFound();
+
+  const cart = await getMyCart();
 
   return (
     <>
@@ -73,6 +76,7 @@ const ProductDetailsPage = async (props: Props) => {
                 {product.stock > 0 && (
                   <div className="flex-center">
                     <AddToCart
+                      cart={cart}
                       item={{
                         productId: product.id,
                         name: product.name,
--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,33 +1,67 @@
 "use client";
 
+import { Loader, Minus, Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
-import { addItemToCart } from "@/lib/actions/cart.actions";
-import { CartItem } from "@/types";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { Cart, CartItem } from "@/types";
 
-type Props = { item: CartItem };
+type Props = {
+  cart?: Cart;
+  item: CartItem;
+};
 
-const AddToCart = ({ item }: Props) => {
+const AddToCart = ({ cart, item }: Props) => {
   const router = useRouter();
 
-  const handleAddToCart = async () => {
-    const addCart = await addItemToCart(item);
+  const [isPending, startTransition] = useTransition();
 
-    if (addCart.success) {
-      toast(`🛒 ${addCart.message}`, {
-        action: {
-          label: "Go to Cart",
-          onClick: () => router.push("/cart")
-        }
-      });
-    }
+  const handleAddToCart = async () => {
+    startTransition(async () => {
+      const addCart = await addItemToCart(item);
+
+      if (addCart.success) {
+        toast.success(`🛒 ${addCart.message}`, {
+          action: {
+            label: "Go to Cart",
+            onClick: () => router.push("/cart")
+          }
+        });
+      }
+    });
   };
 
-  return (
+  const handleRemoveFromCart = async () => {
+    startTransition(async () => {
+      const removeCart = await removeItemFromCart(item.productId);
+
+      if (removeCart.success) {
+        toast.success(`🛒 ${removeCart.message}`);
+      }
+    });
+  };
+
+  const existItem = cart && cart.items.find(x => x.productId === item.productId);
+
+  return existItem ? (
+    <div className="">
+      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+        {isPending ? <Loader className="size-4 animate-spin" /> : <Minus className="size-4" />}
+      </Button>
+
+      <span className="items-center px-2">{existItem.qty}</span>
+
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+        {isPending ? <Loader className="size-4 animate-spin" /> : <Plus className="size-4" />}
+      </Button>
+    </div>
+  ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
-      Add To Cart
+      {isPending ? <Loader className="size-4 animate-spin" /> : <Plus className="size-4" />} Add To
+      Cart
     </Button>
   );
 };
